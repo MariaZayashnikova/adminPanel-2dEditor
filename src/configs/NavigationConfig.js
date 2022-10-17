228252a8abb9015c6458cf760f74bf347f8fc8a0
Mariya_Zaya <mariyazayaschnikova@gmail.com>
2022-10-17 16:00:53 +0300

    pages implemented: userList and Editor

--- src/configs/NavigationConfig.js
+++ src/configs/NavigationConfig.js
@@ -1,19 +1,58 @@
-import { 
-  DashboardOutlined
+import {
+  DashboardOutlined,
+  PlusCircleOutlined,
+  FileTextOutlined
 } from '@ant-design/icons';
 import { APP_PREFIX_PATH } from 'configs/AppConfig'
 
-const dashBoardNavTree = [{
-  key: 'home',
-  path: `${APP_PREFIX_PATH}/home`,
-  title: 'home',
-  icon: DashboardOutlined,
-  breadcrumb: false,
-  submenu: []
-}]
+// const dashBoardNavTree = [{
+//   key: 'home',
+//   path: `${APP_PREFIX_PATH}/home`,
+//   title: 'home',
+//   icon: DashboardOutlined,
+//   breadcrumb: false,
+//   submenu: []
+// }]
+
+const extraNavTree = [
+  {
+    key: 'extra',
+    path: `${APP_PREFIX_PATH}/pages`,
+    title: 'sidenav.pages',
+    icon: PlusCircleOutlined,
+    breadcrumb: true,
+    submenu: [
+      {
+        key: 'extra-pages',
+        path: `${APP_PREFIX_PATH}/pages`,
+        title: 'sidenav.pages',
+        icon: FileTextOutlined,
+        breadcrumb: true,
+        submenu: [
+          {
+            key: 'extra-pages-list',
+            path: `${APP_PREFIX_PATH}/pages/user-list`,
+            title: 'sidenav.pages.userlist',
+            icon: '',
+            breadcrumb: true,
+            submenu: []
+          },
+          {
+            key: 'extra-pages-editor',
+            path: `${APP_PREFIX_PATH}/pages/editor`,
+            title: 'sidenav.pages.editor',
+            icon: '',
+            breadcrumb: false,
+            submenu: []
+          }
+        ]
+      }
+    ]
+  }
+]
 
 const navigationConfig = [
-  ...dashBoardNavTree
+  ...extraNavTree
 ]
 
 export default navigationConfig;
---
+++ src/views/app-views/pages/editor/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Editor = () => {
+    return (
+        <div>
+            Editor component works!
+        </div>
+    )
+}
+
+export default Editor
